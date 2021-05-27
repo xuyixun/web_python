96d92eaa5f39852653379545fb80c8afd45989e3
xuyixun <xuyixun@outlook.com>
2021-05-27 17:44:39 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+import cv2
+from flask import Flask,request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
+    image_original = request.files['file']
+    image_binary = cv2.threshold(image_original,100,255,cv2.THRESH_BINARY)
+    cv2.imwrite('test.png', image_binary)
     return 'Hello, Docker!'
